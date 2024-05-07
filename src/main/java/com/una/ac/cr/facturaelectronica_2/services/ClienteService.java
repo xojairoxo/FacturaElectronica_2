82a30521187304499e8d06a6f1ab3fc902781c7d
xojairoxo <jairo.castillo.aragon@est.una.ac.cr>
2024-05-07 15:42:48 -0600

    TESTING JS
    Is not finding the js script

--- src/main/java/com/una/ac/cr/facturaelectronica_2/services/ClienteService.java
+++ src/main/java/com/una/ac/cr/facturaelectronica_2/services/ClienteService.java
@@ -1,15 +1,17 @@
-package com.una.ac.cr.facturaelectronica_2.services.Clientes;
+package com.una.ac.cr.facturaelectronica_2.services;
 
 import com.una.ac.cr.facturaelectronica_2.data.ClientesRepository;
 import com.una.ac.cr.facturaelectronica_2.logic.ClientesEntity;
+import com.una.ac.cr.facturaelectronica_2.statusHttp.STATUS;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/clientes")
-public class Clientes {
+public class ClienteService {
     @Autowired
     private ClientesRepository clientesRepository;
 
@@ -19,7 +21,12 @@ public class Clientes {
     }
     @GetMapping("{cedula}")
     public ClientesEntity getClientesByCedula(int cedula) {
-        return clientesRepository.findClientesByIdcliente(cedula);
+        try {
+            return clientesRepository.findClientesByIdcliente(cedula);
+        } catch (Exception e) {
+            throw  new ResponseStatusException(STATUS.NOT_FOUND.getStatus() , "Cliente no encontrado");
+        }
+
     }
 
 }
